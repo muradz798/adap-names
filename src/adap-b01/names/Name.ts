4a4d430f51d2254a5ea1f1fb1926f9c52486a1d7
“Murad <murad.zeynalli@fau.de>
2025-10-27 02:20:50 +0100

    implement Name.ts

--- src/adap-b01/names/Name.ts
+++ src/adap-b01/names/Name.ts
@@ -20,53 +20,133 @@ export class Name {
 
     /** Expects that all Name components are properly masked */
     constructor(other: string[], delimiter?: string) {
-        throw new Error("needs implementation or deletion");
+        if(other.length == 0){
+            throw new Error("Kein Kompenent eingegeben");
+        }
+        if(delimiter != undefined){
+            this.delimiter = delimiter;
+        }
+
+        this.components = other
+
+        
+        //throw new Error("needs implementation or deletion");
     }
 
     /**
      * Returns a human-readable representation of the Name instance using user-set control characters
      * Control characters are not escaped (creating a human-readable string)
      * Users can vary the delimiter character to be used
+     * @methodtype conversion-method
      */
     public asString(delimiter: string = this.delimiter): string {
-        throw new Error("needs implementation or deletion");
+        const compLen: number = this.getNoComponents();
+        let stringResult:string;
+        if(compLen>1){
+            stringResult = this.components.join(this.delimiter);
+            return stringResult;
+        }
+        stringResult = this.components[compLen-1]; //hat nur ein Element 
+        return stringResult;
+        
+        //throw new Error("needs implementation or deletion");
     }
 
     /** 
      * Returns a machine-readable representation of Name instance using default control characters
      * Machine-readable means that from a data string, a Name can be parsed back in
      * The control characters in the data string are the default characters
-     */
+     * @methodtype conversion-method
+    */
     public asDataString(): string {
-        throw new Error("needs implementation or deletion");
+        const compLen: number = this.getNoComponents();
+        let currentCompenent:string;
+        let stringResult: string;
+
+        for(let i:number = 0; i<compLen; i++){
+            currentCompenent = this.getComponent(i);
+            if(currentCompenent.includes(DEFAULT_DELIMITER) || currentCompenent.includes(ESCAPE_CHARACTER)){
+                let maskedComponent: string = "";
+                for(let j:number = 0; j<currentCompenent.length; j++){
+                    if(currentCompenent[j] == DEFAULT_DELIMITER){
+                        maskedComponent += `${ESCAPE_CHARACTER}${currentCompenent[j]}`;
+                    }
+                    else if (currentCompenent[j] == ESCAPE_CHARACTER){
+                        maskedComponent += '\\\\';
+                    }
+                    else{
+                        maskedComponent += currentCompenent[j];
+                    }
+                }
+                this.setComponent(i, maskedComponent);
+            }   
+        }
+        stringResult = this.components.join(this.delimiter)
+        return stringResult;
+        
+        //throw new Error("needs implementation or deletion");
     }
 
+
+    /** @methodtype get-method */
     public getComponent(i: number): string {
-        throw new Error("needs implementation or deletion");
+        const compLen = this.getNoComponents();
+        if(i>compLen){
+            throw new Error("Komponent existiert nicht");
+        }
+        return this.components[i]
     }
 
-    /** Expects that new Name component c is properly masked */
+    
+    /** Expects that new Name component c is properly masked 
+     * @methodtype set-method
+    */
     public setComponent(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        const compLen = this.getNoComponents();
+        if(i>compLen){
+            throw new Error("Komponent existiert nicht");
+        }
+        this.components[i] = c;
+    }
+     
+    /** Returns number of components in Name instance 
+     * @methodtype get-method
+    */
+    public getNoComponents(): number {
+        return this.components.length;
+        // throw new Error("needs implementation or deletion");
     }
 
-     /** Returns number of components in Name instance */
-     public getNoComponents(): number {
-        throw new Error("needs implementation or deletion");
-    }
 
-    /** Expects that new Name component c is properly masked */
+    /** Expects that new Name component c is properly masked 
+     * @methodtype set-method
+    */
     public insert(i: number, c: string): void {
-        throw new Error("needs implementation or deletion");
+        const compLen = this.getNoComponents();
+        if(i>compLen){
+            throw new Error("Sie können nicht zu dieser Position irgendwas hinzufügen");
+        }
+        this.components.splice(i, 0, c);
     }
 
-    /** Expects that new Name component c is properly masked */
+    /** Expects that new Name component c is properly masked 
+     * @methodtype command-method
+    */
     public append(c: string): void {
-        throw new Error("needs implementation or deletion");
+        this.components.push(c)
+        //throw new Error("needs implementation or deletion");
     }
 
+    /** @methodtype command-method */
     public remove(i: number): void {
-        throw new Error("needs implementation or deletion");
+        if(i>=this.components.length){
+            throw new Error(`Die gewünschte Komponente darf nicht gelöscht werden, da es nur ${this.components.length} Komponente existieren`);
+        }
+        delete this.components[i]
     }
 
 }
+
+//Objekt erstellen 
+const name = new Name(["a.b.l.t....", "m\\l.z", "c.d"], "/");
+console.log(name.asDataString());
